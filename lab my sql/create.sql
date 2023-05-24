cb04e647b672fa42719b441740a20fc225bc3527
HerminiaPadial <herminiapadialromera@gmail.com>
2023-05-24 19:09:29 +0200

    lab_finished

---
+++ lab my sql/create.sql
@@ -0,0 +1,41 @@
+--CHALLENGE 2 (lab my sql)
+CREATE TABLE Cars2
+(
+	ID INT NOT NULL
+	, VIN VARCHAR(50) NOT NULL
+	, Manufacturer VARCHAR(50) NOT NULL
+	, Model VARCHAR(50) NOT NULL
+	, "Year" INT NOT NULL
+	, Color VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE Customers2
+(
+	ID INT NOT NULL
+	, "Customer ID" INT NOT NULL
+	, Name VARCHAR(50) NOT NULL
+	, Phone VARCHAR(50) NOT NULL
+	, Email VARCHAR(50) NOT NULL
+	, Address VARCHAR(50) NOT NULL
+	, City VARCHAR(50) NOT NULL
+	, "State/Province" VARCHAR(50) NOT NULL
+	, Country VARCHAR(50) NOT NULL
+	, Postal INT NOT NULL
+    );
+
+CREATE TABLE SalesPersons2
+(
+	ID INT NOT NULL
+	, "Staff ID" INT NOT NULL
+	, Name VARCHAR(50) NOT NULL
+	, Store VARCHAR(50) NOT NULL
+);
+CREATE TABLE Invoices2
+(
+	ID INT NOT NULL
+	, "Invoice Number" VARCHAR(50) NOT NULL
+	, "Date" DATE NOT NULL
+	, Car INT NOT NULL
+	, Customer INT NOT NULL
+	, "Sales Person" INT NOT NULL
+);
